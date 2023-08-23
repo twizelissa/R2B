dec7d1669bec87ca3918de6f3417923c3743df9e
gershomlapaix <nsengiyumv33@gmail.com>
2023-08-23 21:25:35 +0200

    footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -17,19 +17,19 @@ export default function Footer() {
            {/* social media links */}
             <div className="flex items-center justify-center gap-6">
                 <Link to={"/#"} className='bg-white text-baseColor rounded-full p-2 transform hover:scale-150 transition-all duration-300'>
-                    <BiLogoFacebook className="text-[1.5rem]"/>
+                    <BiLogoFacebook className="text-[1.5rem] hover:font-semibold"/>
                 </Link>
 
                 <Link to={"/#"} className='bg-white text-baseColor rounded-full p-2 transform hover:scale-150 transition-all duration-300'>
-                    <CiTwitter className="text-[1.5rem]"/>
+                    <CiTwitter className="text-[1.5rem] hover:font-semibold"/>
                 </Link>
                 
                 <Link to={"/#"} className='bg-white text-baseColor rounded-full p-2 transform hover:scale-150 transition-all duration-300'>
-                    <BiLogoInstagram className="text-[1.5rem]"/>
+                    <BiLogoInstagram className="text-[1.5rem] hover:font-semibold"/>
                 </Link>
 
                 <Link to={"/#"} className='bg-white text-baseColor rounded-full p-2 transform hover:scale-150 transition-all duration-300'>
-                    <CiYoutube className="text-[1.5rem]"/>
+                    <CiYoutube className="text-[1.5rem] hover:font-semibold"/>
                 </Link>
                 {/* <div className="">
 
@@ -39,12 +39,35 @@ export default function Footer() {
         
 
         {/* Links */}
-        <div className="flex items-start">
+        <div className="flex items-start gap-32">
             <div className="flex flex-col gap-2">
-                <h1 className='text-[3rem] font-[500]'>IT Solutions</h1>
-                <Link to={"/#"} className='opacity-[0.9]'>Web/App Development</Link>
-                <Link to={"/#"}>Mobile application development</Link>
-                <Link to={"/#"}>UI/UX</Link>
+                <h1 className='text-[2.5rem] font-[500] leading-[6rem]'>IT Solutions</h1>
+                <Link to={"/#"} className='opacity-[0.8]'>Web/App Development</Link>
+                <Link to={"/#"} className='opacity-[0.8]'>Mobile application development</Link>
+                <Link to={"/#"} className='opacity-[0.8]'>UI/UX</Link>
+            </div>
+
+            <div className="flex flex-col gap-2">
+                <h1 className='text-[2.5rem] font-[500] leading-[6rem]'>Ad Monetization</h1>
+                <Link to={"/#"} className='opacity-[0.8]'>SEO/ASO</Link>
+                <Link to={"/#"} className='opacity-[0.8]'>Analytics</Link>
+                <Link to={"/#"} className='opacity-[0.8]'>Google ads</Link>
+                <Link to={"/#"} className='opacity-[0.8]'>Google products</Link>
+            </div>
+
+            <div className="flex flex-col gap-2">
+                <h1 className='text-[2.5rem] font-[500] leading-[6rem]'>Digital Marketing</h1>
+                <Link to={"/#"} className='opacity-[0.8]'>Design</Link>
+                <Link to={"/#"} className='opacity-[0.8]'>Influencer marketing</Link>
+                <Link to={"/#"} className='opacity-[0.8]'>Advertising</Link>
+                <Link to={"/#"} className='opacity-[0.8]'>Video creation</Link>
+            </div>
+
+            <div className="flex flex-col gap-2">
+                <h1 className='text-[2.5rem] font-[500] leading-[6rem]'>Work Preparation</h1>
+                <Link to={"/#"} className='opacity-[0.8]'>Connection</Link>
+                <Link to={"/#"} className='opacity-[0.8]'>Skill Training</Link>
+                <Link to={"/#"} className='opacity-[0.8]'>Learning Bootcamps</Link>
             </div>
         </div>
        </div>
