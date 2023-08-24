ed93dbb7ba8fef715911db9f9bd21adff4260417
gershomlapaix <nsengiyumv33@gmail.com>
2023-08-24 22:47:36 +0200

    services

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -1,8 +1,10 @@
 import {SiToyota,SiMitsubishi, SiSony, SiKyocera, SiNec} from 'react-icons/si'
+import {CgWebsite} from 'react-icons/cg'
 
 import Navigation from '../components/Navigation.js'
 import Footer from '../components/Footer.js'
 import Title from '../components/Title.js'
+import './../styles/animation.css'
 
 import personImage from './../assets/images/on-mac.jpg'
 import partnersImage from './../assets/images/partners-image.svg'
@@ -24,21 +26,33 @@ export default function Services() {
       
       <main>
 
-        <div className="my-40">
+        <div className="my-40 relative">
           <Title title='Our partners'/>
 
         {/* bg image */}
-          <div className="">
-            <img src={partnersImage} alt="" />
+          <div className="bg-white bg-gradient-to-r from-baseColor to-baseColor opacity-[0.2]">
+            <img src={partnersImage} alt="design image" />
           </div>
 
         {/* partners list */}
-        <div className="flex">
-          <div className="">
-          <SiToyota/>
-          <p>Toyota</p>
+          <div className="flex items-start justify-between flex-wrap gap-20 w-full absolute top-[25%]">
+            {[{element:<SiToyota className="text-white text-[6rem]"/>, companyName: 'Toyota'}, 
+            {element: <SiKyocera className="text-white text-[6rem]"/>, companyName: 'Kyocera'},
+            {element: <SiMitsubishi className="text-white text-[6rem]"/>, companyName: 'Mitsubishi'},
+            {element: <SiSony className="text-white text-[6rem]"/>, companyName:'SONY'},
+            {element: <SiNec className="text-white text-[6rem]"/>, companyName: 'NEC Corporation'},
+          ].map((item,index)=>{
+              return(
+                <div className="flex flex-col items-center gap-8 hover:bg-secondColor" key={index}>
+                  <div className="border-2 border-[#8AC5FC] p-28">
+                    {item?.element}
+                  </div>
+                  <p className='text-[#8AC5FC] text-[3rem] hover:animate-moveLabel hover:text-baseColor' >{item.companyName}</p>
+                </div>
+              )
+            })}
           </div>
-        </div>
+        
         </div>
       </main>
 
