9cc68f019e0ac057ec1070466dc419e0cfe69c68
gershomlapaix <nsengiyumv33@gmail.com>
2023-08-30 22:28:48 +0200

    adding descriptions

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -12,8 +12,8 @@ export default function About() {
         <Navigation/>
 
         <div className="flex h-[80vh] relative">
-          <div className="bg-[#8AC5FC] opacity-[0.35] w-[50%] relative -z-50">
-            <h1 className='text-white opacity-[1] absolute top-[60%] md:top-[50%] left-[20%] md:left-[50%] transform -translate-x-[10%] md:-translate-x-[50%] -translate-y-[50%]'>
+          <div className="bg-secondColor opacity-[0.35] w-[50%] relative -z-50">
+            <h1 className='text-white absolute top-[60%] md:top-[50%] left-[20%] md:left-[50%] transform -translate-x-[10%] md:-translate-x-[50%] -translate-y-[50%]'>
               <span className='block text-[6rem]'>ABOUT US</span>
               <span className='block'>Get and use digital services in every corner.</span>
             </h1>
@@ -32,6 +32,47 @@ export default function About() {
       <main>
         <div className="mt-32">
           <Title title='Services Description'/>
+
+          <div className="flex bg-white mt-20 ">
+            <div className="p-20 w-[55%]">
+              <div className="mb-16">
+                <h1 className='font-semibold mb-6'>
+                  <span className='text-secondColor mr-6 text-5xl'>01.</span>
+                  <span>Holistic approach for your media growth</span>
+                </h1>
+                <p className='mx-24 text-[1.5rem]'>Media growth involves in page views improvement and revenue improvement. We provide comprehensive support for SEO and ASO 
+                  to increase traffic and acquire new users, while also assisting in maximizing monetization results through programmatic 
+                  advertising solutions.</p>
+              </div>
+
+              <div className="mb-16">
+                <h1 className='font-semibold mb-6'>
+                  <span className='text-secondColor mr-6 text-5xl'>01.</span>
+                  <span>Influencer Marketing to reach out to more  potential customers</span>
+                </h1>
+                <p className='mx-24 text-[1.5rem]'>
+                  We will find and secure the ideal talent to promote your brand, ranging from nano-influencers with dedicated and 
+                  interactive followers to celebrities capable of showcasing your brand to millions of people.
+                </p>
+              </div>
+
+              <div className="mb-16">
+                <h1 className='font-semibold mb-6'>
+                  <span className='text-secondColor mr-6 text-5xl'>03.</span>
+                  <span>ERP/CRM Development and Integrate</span>
+                </h1>
+                <p className='mx-24 text-[1.5rem]'>
+                  Every project is different and so should the process be. We’ll define a clear framework of how we 
+                  build your own application, website, and softwares to create the best suite for your business by 
+                  exchanging feedback and effectively working together.
+                </p>
+              </div>
+            </div>
+
+            <div className="w-[45%]">
+
+            </div>
+          </div>
         </div>
       </main>
 
