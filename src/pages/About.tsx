afa5af3e4ffc69dc7ed5e14a41e529a67b88e6d6
gershomlapaix <nsengiyumv33@gmail.com>
2023-09-05 08:44:52 +0200

    about page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -2,9 +2,18 @@ import Navigation from '../components/Navigation.js'
 import Footer from '../components/Footer.js'
 
 import aboutusImage from './../assets/images/about-us.png';
+import statisticsImage from './../assets/images/statistics.jpg'
+import motionGraphics from './../assets/images/camera.jpg'
+import softwareDevImage from './../assets/images/software-development.jpg'
+import onFocusImage from './../assets/images/on-focus.jpg'
+import marketImage from './../assets/images/market.jpg'
+import digitalMarketingImage from './../assets/images/digital-marketing.jpg'
+import faqImage from './../assets/images/faq.jpg';
+
 import Title from '../components/Title.js';
 
 export default function About() {
+  const imageStyle = 'w-full h-full rounded-xl object-cover overflow-hidden'
   return (
     <div>
 {/* -------- Navigatoin */}
@@ -47,7 +56,7 @@ export default function About() {
 
               <div className="mb-16">
                 <h1 className='font-semibold mb-6'>
-                  <span className='text-secondColor mr-6 text-5xl'>01.</span>
+                  <span className='text-secondColor mr-6 text-5xl'>02.</span>
                   <span>Influencer Marketing to reach out to more  potential customers</span>
                 </h1>
                 <p className='mx-24 text-[1.5rem]'>
@@ -69,10 +78,39 @@ export default function About() {
               </div>
             </div>
 
-            <div className="w-[45%]">
+            <div className="w-[45%] p-20 relative">
+              <div className="flex items-center gap-8">
+                <div className="flex flex-col gap-8 w-[33%] h-[65vh]">
+                  {/* first images */}
+                  <img src={statisticsImage} className={imageStyle} alt="statistics" />
+                  <img src={digitalMarketingImage} className={imageStyle} alt="Dig marketing" />
+                  <img src={marketImage} className={imageStyle} alt="Marketing" />
+                </div>
 
+                {/* second images */}
+                <div className="flex flex-col gap-8 w-[33%] h-[45vh]">
+                  <img src={softwareDevImage} className={imageStyle} alt="Software development" />
+                  <img src={motionGraphics} className={imageStyle} alt="Motion graphics" />
+                </div>
+
+                {/* Third image */}
+                <div className="w-[33%] h-[25vh]">
+                  <img src={onFocusImage} className={imageStyle} alt="On focus" />
+                </div>
+              </div>
+              <div className="bg-secondColor opacity-[0.35] absolute top-0 right-0 h-full w-full"></div>
             </div>
           </div>
+        </div>
+
+        <div className="mt-32 relative">
+          <div className="w-full ">
+            <img src={faqImage} alt="faq" />
+          </div>
+
+          <div className="bg-baseColor opacity-[0.70] absolute top-0 left-0 h-full w-full"></div>
+
+          <Title title='FAQ'/>
         </div>
       </main>
 
